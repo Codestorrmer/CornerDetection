5ac6bbf412654f98b6f5909bbda80e79686f2c56
Arjun Sripathy <arjun.sripathy2000@gmail.com>
2017-07-07 17:33:11 +0000

    better corner

--- CornerD.py
+++ CornerD.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-#from sweeppy import Sweep
+from sweeppy import Sweep
 import itertools
 
 #True->use LiDAR, False->use text files
@@ -11,16 +11,13 @@ MODE = False
 XYSMOOTH = 0
 #Smooth on Derivative
 DSMOOTH = 0
-#Cutoff for trimming to sharp derivative change
-#i.e if =0.1 we trim away derivative changes that are less than 1/10 biggest change from sides
-TRIMCUTOFF = 0.2
 #Angle of corner we want to detect(for boiler corner set to 45)
 CORNERDETECT = 90
 #Buffer we allow for corner so if cornerdetect=45 cornerbuffer=5 we look for 40-50 deg
 CORNERBUFFER = 20
 
 #Target: approx degree of corner
-TARGET = 240
+TARGET = 160
 #angle buffer of search either way
 TARGETBUFFER = 10
 
@@ -56,7 +53,7 @@ def within(a):
 	if(startAngle>endAngle):
 		return a>startAngle or a<endAngle
 
-'''if(MODE):
+if(MODE):
     print("Using LiDAR")
     with Sweep('/dev/ttyUSB0') as sweep:
         sweep.set_motor_speed(2)
@@ -76,7 +73,7 @@ def within(a):
                 break
             first = False
 
-        sweep.stop_scanning()'''
+        sweep.stop_scanning()
 
 if(not MODE):
 	goodInd = []
@@ -236,68 +233,47 @@ for i in range(len(dist)):
 
 if CORNER2D:plt.scatter(xdata[cIndex],ydata[cIndex])
 
-def trimA(slopes, dists):
-	maxVal = 0
-	for s in slopes:
-		if(abs(s)>maxVal):maxVal=abs(s)
-	c = TRIMCUTOFF*maxVal
-	while(abs(slopes[0])<c):
-		slopes.pop(0)
-		dists.pop(0)
-	while(abs(slopes[len(slopes)-1])<c):
-		slopes.pop(len(slopes)-1)
-		dists.pop(len(dists)-1)
-	midVal = (dists[0]+dists[len(dists)-1])/2
-	return midVal
-
 slopeTotals = []
 sign = (sD[1]-sD[0])>0
 xSlope = []
 mx = 0
 
 thisSlopes = [sD[1]-sD[0]]
-thisDists = [dist[0],dist[1]]
+maxThisSlope = 0
+maxCorner = 0
 
 for i in range(1,length-1):
 	thisSlope = sD[i+1]-sD[i]
 	thisSign = thisSlope>0
 	if(sign==thisSign):
 		thisSlopes.append(thisSlope)
-		thisDists.append(dist[i+1])
+		if(abs(thisSlope)>maxThisSlope):
+                        maxThisSlope = abs(thisSlope)
+                        maxCorner = i+1
 	else:
-		midVal = trimA(thisSlopes,thisDists)
 		slopeTotals.append(sum(thisSlopes))
 		aL = abs(sum(thisSlopes))
-		thisSlopes = [thisSlope]
-		thisDists = [dist[i],dist[i+1]]
 		if(aL>mx):
 			mx = aL
 		sign = thisSign
-		xSlope.append(midVal)
+		xSlope.append(maxCorner)
+		thisSlopes = [thisSlope]
+		maxThisSlope = abs(thisSlope)
+		maxCorner = i+1
 
-midVal = trimA(thisSlopes,thisDists)
 slopeTotals.append(sum(thisSlopes))
 aL = abs(sum(thisSlopes))
 if(aL>mx):
 	mx = aL
-xSlope.append(midVal)
+xSlope.append(maxCorner)
 
-corners = []
-
-for i in range(0,len(slopeTotals)):
-	if(abs(slopeTotals[i]-CORNERDETECT)<CORNERBUFFER):
-		corners.append(xSlope[i])
-
-cornerOn = 0
 cornerX = []
 cornerY = []
 
-for i in range(0,len(dist)):
-	if(cornerOn>=len(corners)):break
-	if(dist[i]>corners[cornerOn]):
-		cornerX.append((xdata[i+1]+xdata[i])/2)
-		cornerY.append((ydata[i+1]+ydata[i])/2)
-		cornerOn+=1
+for i in range(0,len(slopeTotals)):
+	if(abs(slopeTotals[i]-CORNERDETECT)<CORNERBUFFER):
+                cornerX.append(xdata[xSlope[i]])
+                cornerY.append(ydata[xSlope[i]])
 
 
 if CORNERST:plt.scatter(cornerX, cornerY)
